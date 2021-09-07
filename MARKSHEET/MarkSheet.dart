7343c1b1c367e5a7fadd3e3c4a6ff5b01c9d6798
atharsajd <athersajid620@gmail.com>
2021-09-07 21:06:54 +0500

    update marksheet

--- MARKSHEET/MarkSheet.dart
+++ MARKSHEET/MarkSheet.dart
@@ -1,42 +1,50 @@
 void main() {
-  
-  // Total mark is 100 of all Subject 
+  //STUDENT DETAIL
+  var name = "Ahmed";
+  var fathername = "Muhammad Nisar";
+  var roll = 1234;
+  var group = "sceince";
+
+  print("Name : $name");
+  print("Father name : $fathername");
+  print("Roll No : $roll");
+  print("Group : $group");
+  print("");
+
+  // Total mark is 100 of all Subject
   var bio = 75;
-  var mat = 95;
-  var chem = 85;
-  var isl = 70;
+  var mat = 89;
+  var chem = 65;
+  var isl = 94;
   var urd = 91;
+
+  print("SUBJECTS");
+  print("Biology : $bio");
+  print("Mathmetics : $mat");
+  print("Chemistry : $chem");
+  print("Islamiat : $isl");
+  print("Urdu : $urd");
+  print("");
+
   var totalmarks = 500;
   var obtainmarks = bio + mat + chem + isl + urd;
-  
-  var percentage = obtainmarks/totalmarks * 100;
-  
+
+  var percentage = obtainmarks / totalmarks * 100;
+
   print("Obtain Marks $obtainmarks");
   print("Percentage $percentage%");
-  
-  if(percentage>=80){
+
+  if (percentage >= 80) {
     print("A One Grade");
-  }
-  
-  else if(percentage>=70){
-    print ("A Grade");
-  }
-  
-  else if (percentage>=60){
-    print ("B Grade");
-  }
-  
-  else if (percentage>=50){
-    print ("C Grade");
-  }
-  
-  else if (percentage>=40){
-    print ("PASS");
-  }
-  
-  else{
+  } else if (percentage >= 70) {
+    print("A Grade");
+  } else if (percentage >= 60) {
+    print("B Grade");
+  } else if (percentage >= 50) {
+    print("C Grade");
+  } else if (percentage >= 40) {
+    print("PASS");
+  } else {
     print("Fail");
   }
-  
-  
 }
