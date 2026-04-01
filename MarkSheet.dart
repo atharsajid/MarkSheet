fddb5b958404ffab7b1388b89ab1c6015755e13c
MUHAMMAD ATHAR SAJID <90137896+atharsajid@users.noreply.github.com>
2026-04-01 21:34:34 +0500

    Update MarkSheet.dart

--- MarkSheet.dart
+++ MarkSheet.dart
@@ -5,10 +5,10 @@ void main() {
   var roll = 1234;
   var group = "sceince";
 
-  print("Name \t \t : \t $name");
-  print("Father name \t : \t $fathername");
-  print("Roll No \t : \t $roll");
-  print("Group \t \t : \t $group");
+  print("Name \t \t \t \t : \t $name");
+  print("Father name \t: \t $fathername");
+  print("Roll No \t \t \t: \t $roll");
+  print("Group \t \t \t \t: \t $group");
   print("");
 
   // Total mark is 100 of all Subject
@@ -19,19 +19,19 @@ void main() {
   var urd = 91;
 
   print("SUBJECTS");
-  print("Biology \t : \t $bio");
-  print("Mathmetics \t : \t $mat");
-  print("Chemistry \t : \t $chem");
-  print("Islamiat \t : \t $isl");
-  print("Urdu \t \t : \t $urd \n");
+  print("Biology      : \t $bio");
+  print("Mathmetics   : \t $mat");
+  print("Chemistry    : \t $chem");
+  print("Islamiat     : \t $isl");
+  print("Urdu         : \t $urd \n");
 
   var totalmarks = 500;
   var obtainmarks = bio + mat + chem + isl + urd;
 
   var percentage = obtainmarks / totalmarks * 100;
 
-  print("Obtain Marks \t : \t $obtainmarks");
-  print("Percentage \t : \t $percentage% \n");
+  print("Obtain Marks : \t $obtainmarks");
+  print("Percentage   : \t $percentage% \n");
 
   if (percentage >= 80) {
     print("A One Grade");
